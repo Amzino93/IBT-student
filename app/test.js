4b17ef8c020f6ae76f37a2f9dbf2784da75ca69b
amza simpore <amzasimpore@gmail.com>
2022-11-12 10:28:13 -0600

    adding test.js

---
+++ app/test.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const assert = require('assert');
+
+describe('Simple test', function() {
+
+    it('bar equal bar', function() {
+        assert.equal('bar', 'bar', '`bar` equal `bar`');
+    });
+
+    it('foo equal foo', function() {
+        assert.equal('foo', 'foo', '`foo` equal `foo`');
+    });
+
+    it('foo not equal bar', function() {
+        assert.notEqual('foo', 'bar', '`foo` not equal `bar`');
+    });
+});
+Footer
+© 2022 GitHub, Inc.
